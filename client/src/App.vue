fee6f84aa80eee8a85809fc89f8e0aced9ca334f
SJ <86448785+SamerJurdi@users.noreply.github.com>
2025-04-12 00:37:58 +0300

    add router logic and move login logic into a separate view

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,16 +1,7 @@
 <script setup lang="ts">
-import Login from '@/components/Login.vue';
-
-const login = async (username: string, password: string): Promise<string | null> => {
-
-  if (username !== 'admin' || password !== 'password123') {
-    return 'Invalid username or password';
-  } else console.log('Attempting login with:', username, password);
-
-  return null;
-};
+import {RouterView} from 'vue-router';
 </script>
 
 <template>
-  <Login :login="login" />
+  <RouterView />
 </template>
---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import Login from '@/components/Login.vue';
+
+const login = async (username: string, password: string): Promise<string | null> => {
+
+  if (username !== 'admin' || password !== 'password123') {
+    return 'Invalid username or password';
+  } else console.log('Attempting login with:', username, password);
+
+  return null;
+};
+</script>
+
+<template>
+  <Login :login="login" />
+</template>
