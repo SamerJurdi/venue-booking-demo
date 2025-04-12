71b3ecc5ca3e55ce3fb7cf41156123f326b3a8b6
SJ <86448785+SamerJurdi@users.noreply.github.com>
2025-04-12 20:52:21 +0300

    refactor files format using prettier rules

--- client/src/containers/LoginContainer.vue
+++ client/src/containers/LoginContainer.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import axios from 'axios';
+import axios from 'axios'
 import { LogInForm } from '@/components'
 
 const props = defineProps({
@@ -10,27 +10,28 @@ const props = defineProps({
 })
 
 const login = async (username: string, password: string): Promise<string | null> => {
-  let message = null;
+  let message = null
   if (username.trim() === '' || password.trim() === '') {
     return 'Invalid username or password'
   } else {
     console.log('Attempting login with:')
-    await axios.post(`/api/auth/login`, {
-      username,
-      password
-    })
-    .then(function (response) {
-      if (response.status === 200) {
-        props.onLogin()
-      } else {
-        message =  'Log in failed. Status: ' + response.status;
-      }
-    })
-    .catch(function (error) {
-      console.log({error});
-      message = error.response.data;
-    });
-    return message;
+    await axios
+      .post(`/api/auth/login`, {
+        username,
+        password,
+      })
+      .then(function (response) {
+        if (response.status === 200) {
+          props.onLogin()
+        } else {
+          message = 'Log in failed. Status: ' + response.status
+        }
+      })
+      .catch(function (error) {
+        console.log({ error })
+        message = error.response.data
+      })
+    return message
   }
 }
 </script>
