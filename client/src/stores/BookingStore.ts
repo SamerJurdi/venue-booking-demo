689bd94f1b111319e189d7750aa0d4cb38c9161c
SJ <86448785+SamerJurdi@users.noreply.github.com>
2025-04-13 16:48:19 +0300

    added demo state on the client

--- client/src/stores/BookingStore.ts
+++ client/src/stores/BookingStore.ts
@@ -9,7 +9,27 @@ const getInitialState = (): {
   return {
     selectedDate: new Date(),
     newEventDate: new Date(),
-    events: {},
+    events: {
+      '2025-04-13': [
+        {
+          title: 'Test 1',
+          start: '09:30',
+          end: '10:00',
+        },
+        {
+          title: 'Test 2',
+          start: '11:30',
+          end: '12:00',
+        },
+      ],
+      '2025-04-14': [
+        {
+          title: 'Test 3',
+          start: '16:00',
+          end: '17:00',
+        },
+      ],
+    },
   }
 }
 
@@ -17,7 +37,6 @@ export const useBookingStore = defineStore('booking', {
   state: getInitialState,
   actions: {
     addEvent(date: Date, event: EventItem) {
-      console.log({ date })
       const key = date.toISOString().split('T')[0]
       if (!this.events[key]) {
         this.events[key] = []
