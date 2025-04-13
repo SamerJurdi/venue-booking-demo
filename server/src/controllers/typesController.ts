de9018b2b0188bd89197e4a79184b5c09409ed6e
SJ <86448785+SamerJurdi@users.noreply.github.com>
2025-04-13 22:19:55 +0300

    added an API to get reservation types

---
+++ server/src/controllers/typesController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { queryDatabase } from '../config/db.js';
+
+async function queryReservationTypes() {
+  const sql = 'SELECT * FROM "Type" WHERE parent_type_id = $1';
+  return await queryDatabase(sql, ['reservationTypes']);
+}
+
+async function getReservationTypes(req: Request, res: Response) {
+  try {
+    const reservationTypes = await queryReservationTypes();
+    res.status(200).json({reservationTypes})
+  } catch (error) {
+    console.error('Error fetching reservation types:', error);
+    res.status(500).json({
+      error: 'Internal server error while retrieving reservation types',
+    });
+  }
+}
+
+export {getReservationTypes}
--- server/src/routes/reservationRoute.ts
+++ server/src/routes/reservationRoute.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import { ensureSessionActive } from '../middleware/sessionManger.js';
 import { createReservation, getReservations } from '../controllers/bookingController.js';
+import { getReservationTypes } from '../controllers/typesController.js';
 
 const router = Router();
 
 router.get('/', ensureSessionActive, getReservations);
 router.post('/create', ensureSessionActive, createReservation);
+router.get('/types', getReservationTypes);
 
 export default router;
