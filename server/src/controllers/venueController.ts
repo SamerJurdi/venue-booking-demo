1e013f1b738bad43e89ea5c0a31943a9554cdc71
SJ <86448785+SamerJurdi@users.noreply.github.com>
2025-04-16 02:41:16 +0300

    created get venue specific reservation API

--- server/src/controllers/venueController.ts
+++ server/src/controllers/venueController.ts
@@ -6,6 +6,11 @@ async function queryVenues() {
   return await queryDatabase(sql);
 }
 
+async function queryVenuReservations(venueId: string) {
+  const sql = 'SELECT * FROM "ReservationAndOrganizer" WHERE venue_id = $1';
+  return await queryDatabase(sql, [venueId]);
+}
+
 async function getVenues(req: Request, res: Response) {
   try {
     let venues = await queryVenues();
@@ -19,4 +24,17 @@ async function getVenues(req: Request, res: Response) {
   }
 }
 
-export {getVenues}
+async function getVenueReservations(req: Request, res: Response) {
+  const venueId = req.params.venueId;
+  try {
+    const reservations = await queryVenuReservations(venueId);
+    res.status(200).json({ reservations });
+  } catch (error) {
+    console.error('Error fetching reservations:', error);
+    res.status(500).json({
+      message: 'Internal server error while retrieving reservations',
+    });
+  }
+}
+
+export {getVenues, getVenueReservations}
