85d38fed4395ff93782365367f00f5373af60bf0
SJ <86448785+SamerJurdi@users.noreply.github.com>
2025-04-17 04:52:41 +0300

    moved api rewrites on the front end server to a vercel json to work in production

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -17,7 +17,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://venue-booking-backend.vercel.app/', // 'http://localhost:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
       }
     }
